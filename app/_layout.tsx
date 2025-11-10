5775279d67753b316995b418d6ac37dd21e84948
Kris Brooks <kbrooks8193@gmail.com>
2025-11-10 12:40:33 -0500

    Updated made to hide status bar on app state changes.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,34 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "react-native";
-import './globals.css';
+import { StatusBar, setStatusBarHidden } from "expo-status-bar";
+import { useEffect } from "react";
+import { AppState } from "react-native";
+import "./globals.css";
 
 export default function RootLayout() {
+  useEffect(() => {
+    setStatusBarHidden(true, "slide");
+
+    const sub = AppState.addEventListener("change", (state) => {
+      if (state === "active" || state === "inactive") {
+        setStatusBarHidden(true, "slide");
+      }
+    });
+
+    return () => sub.remove();
+  }, []);
+
   return (
-  <>
-    <StatusBar hidden={true} />
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="movies/[id]"
-        options={{ headerShown: false }}
-      />
-    </Stack>
-  </>
+    <>
+      <StatusBar hidden animated />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          statusBarHidden: true,
+        }}
+      >
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
